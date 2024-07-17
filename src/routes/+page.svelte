ab0d92c7cf9f3637a12a231d222b9a4d45afbaa6
Traw <kirito.san1253@gmail.com>
2024-07-17 16:51:47 +0300

    contered white rectangle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,5 @@
     <title>آلة حاسبة</title>
 </svelte:head>
 
-<div
-    class="bg-blue-950 h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center min-h-[50dvh]"
->
-    <div class="bg-blue-600 h-[5rem] w-[5rem] rounded-3xl"></div>
+<div class="bg-blue-950 h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center min-h-[50dvh]">
 </div>
