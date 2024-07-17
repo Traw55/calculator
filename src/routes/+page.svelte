0f2151e613f5ebd97c699863282225f024027b7b
Traw <kirito.san1253@gmail.com>
2024-07-17 21:56:00 +0300

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,26 +2,31 @@
     <title>آلة حاسبة</title>
 </svelte:head>
 
-<div class="bg-blue-950  rounded-3xl grid
- grid-cols-4 gap-1">
-    <button> % </button>
-    <button> √ </button>
-    <button> CE </button>
-    <button> C </button>
+<div class="bg-blue-200  rounded-3xl grid
+ grid-cols-4 gap-1 p-6 font-semibold text-xl shadow-2xl">
+
+    <div class="bg-blue-500 rounded-full col-span-4 h-12 flex items-center 
+ px-4 mb-2 text-white">
+    12345
+ </div>
+    <button class="bg-[#f3f6fc]"> % </button>
+    <button class="bg-[#f3f6fc]"> √ </button>
+    <button class="bg-[#f3f6fc]"> CE</button>
+    <button class="bg-[#1f2b54] text-white"> C </button>
     <button> 7 </button>
     <button> 8 </button>
     <button> 9 </button>
-    <button> - </button>
+    <button class="bg-[#fd3f59] text-white"> - </button>
     <button> 4 </button>
     <button> 5 </button>
     <button> 6 </button>
-    <button> ÷ </button>
+    <button class="bg-[#2285fd] text-white"> ÷ </button>
     <button> 1 </button>
     <button> 2 </button>
     <button> 3 </button>
-    <button> × </button>
+    <button class="bg-[#f9c80e] text-white"> × </button>
     <button> . </button>
     <button> 0 </button>
-    <button> = </button>
-    <button> + </button>
+    <button class="bg-[#f3f6fc]"> = </button>
+    <button class="bg-[#63dc74] text-white"> + </button>
 </div>
