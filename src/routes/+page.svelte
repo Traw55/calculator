0d1b9711a877647e8518cba640f01cd7e5990aaa
Traw <kirito.san1253@gmail.com>
2024-07-17 17:38:36 +0300

    bottons using grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,26 @@
     <title>آلة حاسبة</title>
 </svelte:head>
 
-<div class="bg-blue-950 h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center min-h-[50dvh]">
+<div class="bg-blue-950  rounded-3xl grid
+ grid-cols-4 gap-1">
+    <button> % </button>
+    <button> √ </button>
+    <button> CE </button>
+    <button> C </button>
+    <button> 7 </button>
+    <button> 8 </button>
+    <button> 9 </button>
+    <button> - </button>
+    <button> 4 </button>
+    <button> 5 </button>
+    <button> 6 </button>
+    <button> ÷ </button>
+    <button> 1 </button>
+    <button> 2 </button>
+    <button> 3 </button>
+    <button> × </button>
+    <button> . </button>
+    <button> 0 </button>
+    <button> = </button>
+    <button> + </button>
 </div>
