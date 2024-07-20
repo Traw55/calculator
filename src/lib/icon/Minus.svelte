98ccd89dce518c882676ed5ddf6d6b3ab7689124
Traw <kirito.san1253@gmail.com>
2024-07-20 13:20:14 +0300

    everything is working now

---
+++ src/lib/icon/Minus.svelte
@@ -0,0 +1,3 @@
+<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24"
+    ><path fill="currentColor" d="M19 13H5v-2h14z" /></svg
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,65 @@
+<script lang="ts">
+   import Plus from'$lib/icon/Plus.svelte';
+   import Multiply from'$lib/icon/Multiply.svelte';
+   import Minus from'$lib/icon/Minus.svelte';
+   import Division from'$lib/icon/Division.svelte';
+   import BackspaceIcon from'$lib/icon/Backspace.svelte';
+
+   function clear(){
+      equation = "";
+   }
+
+   function backspace(){
+      equation = equation.substring(0, equation.length - 1);
+   }
+   
+   function addToEquation(value : string){
+      equation += value;
+   }
+
+   function solve(){
+      equation = eval(equation);
+   }
+
+   let equation: string = "";
+</script>
+
 <svelte:head>
     <title>آلة حاسبة</title>
 </svelte:head>
 
-<div class="bg-blue-200  rounded-3xl grid
- grid-cols-4 gap-1 p-6 font-semibold text-xl shadow-2xl">
+<div class="bg-blue-200  rounded-3xl grid grid-cols-4 gap-1 p-6 font-semibold text-xl shadow-2xl">
 
-    <div class="bg-blue-500 rounded-full col-span-4 h-12 flex items-center 
- px-4 mb-2 text-white">
-    12345
- </div>
-    <button class="bg-[#f3f6fc]"> % </button>
-    <button class="bg-[#f3f6fc]"> √ </button>
-    <button class="bg-[#f3f6fc]"> CE</button>
-    <button class="bg-[#1f2b54] text-white"> C </button>
-    <button> 7 </button>
-    <button> 8 </button>
-    <button> 9 </button>
-    <button class="bg-[#fd3f59] text-white"> - </button>
-    <button> 4 </button>
-    <button> 5 </button>
-    <button> 6 </button>
-    <button class="bg-[#2285fd] text-white"> ÷ </button>
-    <button> 1 </button>
-    <button> 2 </button>
-    <button> 3 </button>
-    <button class="bg-[#f9c80e] text-white"> × </button>
-    <button> . </button>
-    <button> 0 </button>
-    <button class="bg-[#f3f6fc]"> = </button>
-    <button class="bg-[#63dc74] text-white"> + </button>
+<div class="bg-blue-500 rounded-full col-span-4 h-12 flex items-center px-4 mb-2 text-white">
+    {equation}
+</div>
+    <button on:click={clear} class="bg-blue-950 text-white"> AC </button>
+    <button on:click={backspace} class="bg-[#f3f6fc]">
+      <BackspaceIcon />
+    </button>
+    <button on:click={() => addToEquation(" /100")} class="bg-[#f3f6fc]"> % </button>
+    <button on:click={() => addToEquation(" + ")} class="bg-[#63dc74] text-white"> 
+      <Plus/> 
+    </button>
+    <button on:click={() => addToEquation("7")}> 7 </button>
+    <button on:click={() => addToEquation("8")}> 8 </button>
+    <button on:click={() => addToEquation("9")}> 9 </button>
+    <button on:click={() => addToEquation(" - ")} class="bg-[#fd3f59] text-white">
+       <Minus />
+    </button>
+    <button on:click={() => addToEquation("4")}> 4 </button>
+    <button on:click={() => addToEquation("5")}> 5 </button>
+    <button on:click={() => addToEquation("6")}> 6 </button>
+    <button on:click={() => addToEquation(" / ")} class="bg-[#2285fd] text-white"> 
+      <Division />
+    </button>
+    <button on:click={() => addToEquation("1")}> 1 </button>
+    <button on:click={() => addToEquation("2")}> 2 </button>
+    <button on:click={() => addToEquation("3")}> 3 </button>
+    <button on:click={() => addToEquation(" * ")} class="bg-[#f9c80e] text-white"> 
+      <Multiply /> 
+    </button>
+    <button on:click={() => addToEquation(".")}> . </button>
+    <button on:click={() => addToEquation("0")}> 0 </button>
+    <button on:click={solve} class="col-span-2 bg-[#f3f6fc]"> = </button>
 </div>
